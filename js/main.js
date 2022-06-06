6ab01662dabc8696867e5b8192de42540fe2a366
Ravi <chauhan.ravi100891@gmail.com>
2022-06-06 14:25:22 +0530

    Minor changes

--- js/main.js
+++ js/main.js
@@ -12,76 +12,76 @@ const closeSidebarBtn = document.querySelector('.close-sidebar');
 // -- Intersection Observer  -------------------------------------------------------------------
 
 const options = {
-    threshold: 1
+	threshold: 1
 };
 
 const appearOptions = {
-    threshold: 0.8,
-    rootMargin: '-100px 0px -100px 0px'
+	threshold: 0.8,
+	rootMargin: '-100px 0px -100px 0px'
 };
 
 const headerObserver = new IntersectionObserver(entries => {
 
-    entries.forEach(entry => {
+	entries.forEach(entry => {
 
-        if (!entry.isIntersecting) {
-            header.classList.add('sticky');
-        } else {
-            header.classList.remove('sticky');
-        }
-    });
+		if (!entry.isIntersecting) {
+			header.classList.add('sticky');
+		} else {
+			header.classList.remove('sticky');
+		}
+	});
 }, options);
 
 const scrollObserver = new IntersectionObserver(entries => {
 
-    entries.forEach(entry => {
+	entries.forEach(entry => {
 
-        if (!entry.isIntersecting) {
-            scrollTop.style.right = "3%";
-        } else {
-            scrollTop.style.right = "-30%";
-        }
+		if (!entry.isIntersecting) {
+			scrollTop.style.right = "3%";
+		} else {
+			scrollTop.style.right = "-30%";
+		}
 
-    });
+	});
 }, options);
 
 let appearObserver = new IntersectionObserver(function (entries) {
 
-    entries.forEach(entry => {
+	entries.forEach(entry => {
 
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            entry.target.classList.add('appear');
-            appearObserver.unobserve(entry.target);
-        }
-    });
+		if (!entry.isIntersecting) {
+			return;
+		} else {
+			entry.target.classList.add('appear');
+			appearObserver.unobserve(entry.target);
+		}
+	});
 }, appearOptions);
 
 headerObserver.observe(bannerSection);
 scrollObserver.observe(bannerSection);
 
 fadeIns.forEach(fadeIn => {
-    appearObserver.observe(fadeIn);
+	appearObserver.observe(fadeIn);
 });
 
 
 // -- Event Listeners -------------------------------------------------------------------
 
 const openSidebar = () => {
-    sidebar.style.right = "0";
+	sidebar.style.right = "0";
 };
 
 const closeSidebar = () => {
-    sidebar.style.right = "-100%";
+	sidebar.style.right = "-100%";
 };
 
 scrollTop.addEventListener('click', () => {
 
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
+	window.scrollTo({
+		top: 0,
+		behavior: "smooth"
+	});
 });
 
 hamburgerMenu.addEventListener('click', openSidebar);
@@ -94,57 +94,57 @@ closeSidebarBtn.addEventListener('click', closeSidebar);
 
 const clientSwiper = new Swiper('#client-list', {
 
-    loop: true,
-    keyboard: true,
-    spaceBetween: 10,
-    slidesPerView: 7,
-    centeredSlides: true,
-    breakpoints: {
-        100: {
-            slidesPerView: 2
-        },
-        460: {
-            slidesPerView: 3
-        },
-        768: {
-            slidesPerView: 5
-        },
-        1260: {
-            slidesPerView: 7
-        }
-    },
+	loop: true,
+	keyboard: true,
+	spaceBetween: 10,
+	slidesPerView: 7,
+	centeredSlides: true,
+	breakpoints: {
+		100: {
+			slidesPerView: 2
+		},
+		460: {
+			slidesPerView: 3
+		},
+		768: {
+			slidesPerView: 5
+		},
+		1260: {
+			slidesPerView: 7
+		}
+	},
 
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false
-    }
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false
+	}
 });
 
 const instagramSwiper = new Swiper('#intagram-post-list', {
 
-    loop: true,
-    keyboard: true,
-    spaceBetween: 20,
-    slidesPerView: 5,
-    centeredSlides: true,
+	loop: true,
+	keyboard: true,
+	spaceBetween: 20,
+	slidesPerView: 5,
+	centeredSlides: true,
 
-    breakpoints: {
-        100: {
-            slidesPerView: 1
-        },
-        576: {
-            slidesPerView: 3
-        },
-        768: {
-            slidesPerView: 4
-        },
-        1200: {
-            slidesPerView: 5
-        }
-    },
+	breakpoints: {
+		100: {
+			slidesPerView: 1
+		},
+		576: {
+			slidesPerView: 3
+		},
+		768: {
+			slidesPerView: 4
+		},
+		1200: {
+			slidesPerView: 5
+		}
+	},
 
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false
-    }
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false
+	}
 });
